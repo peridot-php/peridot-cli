4e209cce4f14f9f33caf4f523841adada62ae4e8
Brian Scaturro <scaturrob@gmail.com>
2015-05-18 20:36:37 -0400

    pass context to factory

--- src/Application.php
+++ src/Application.php
@@ -89,9 +89,9 @@ class Application extends ConsoleApplication
      */
     public function getPeridotCommand($output)
     {
-        $factory = new ReporterFactory($output, $this->environment->getEventEmitter());
         $emitter = $this->environment->getEventEmitter();
         $context = $this->environment->getContext();
+        $factory = new ReporterFactory($output, $emitter, $context);
         return new Command($factory, $emitter, $context);
     }
 
